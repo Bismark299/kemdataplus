b27f576929f0cae99f57d1e903e16f18463912c8
Oteng Kwame Bismark <otengbismark7@gmail.com>
2026-02-23 01:21:19 +0000

    Fix: increase pagination validator max limit to 10000

--- server/src/middleware/validators.js
+++ server/src/middleware/validators.js
@@ -84,7 +84,7 @@ const settingsValidation = [
 // Pagination validator
 const paginationValidation = [
   query('page').optional().isInt({ min: 1 }).toInt(),
-  query('limit').optional().isInt({ min: 1, max: 1000 }).toInt(),
+  query('limit').optional().isInt({ min: 1, max: 10000 }).toInt(),
   validate
 ];
 
